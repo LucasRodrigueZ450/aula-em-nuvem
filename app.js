ff2735cba11baf731e1a6e628fe5cdecc4160786
Lucas Rodrigues <lucasrodriguessl10@gmail.com>
2024-04-22 19:40:06 -0300

    upando alterações

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require (' express ');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Página inicial</h1>');
+    res.send('<h1>Página inicial alterado men</h1>');
 });
 
 app.get('/sobre', (req, res) => {
